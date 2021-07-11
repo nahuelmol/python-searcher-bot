79b9b504c263778e66ef9f9c4b70ee7511ca52ad
Silvio Molina <molinahuel44@gmail.com>
2021-07-10 23:25:28 -0300

    nothing, I just proved

---
+++ searcher/prove.py
@@ -0,0 +1,4 @@
+import searcher_algory
+
+if __name__ == '__main__':
+	searcher_algory.searching('Lauda')
--- searcher/searcher_algory.py
+++ searcher/searcher_algory.py
@@ -1,20 +1,21 @@
 from googlesearch import search
 import requests, sys
-from . import shorter
+import shorter
 
 def searching(mystring):
-	pages_scoped = 10
-	query = mystring
-	pages = []
-	resulted = []
-	j = 0
+		pages_scoped = 10
+		query = mystring
+		pages = []
+		resulted = []
+		j = 0
+	
+		resulted = search(query, num_results=pages_scoped)
+	
+		for i in resulted:
+			link_cutered = shorter.shorter_links(i)
+			print(link_cutered)
+			pages.append(i)
+		return pages
 
-	resulted = search(query, num_results=pages_scoped)
-
-	for i in resulted:
-		link_cutered = shorter.shorter_links(i)
-		print(link_cutered)
-		pages.append(i)
-	return pages
-
-
+#if __name__ == "searcher_algory":
+#	searching('lauda')
